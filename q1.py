7941a84c3e692f601974edc00c18542bdea4ca85
WhoAdarshPandya <adarshpandya0@gmail.com>
2020-05-02 20:59:06 +0530

    second

--- q1.py
+++ q1.py
@@ -1,7 +1,11 @@
-import tkinter
+import tkinter as tk
 
 print("hello world")
-window = tkinter.Tk()
-window.title("Pixxa")
-
+window = tk.Tk()
+canvas = tk.Canvas(window,width=300,height=300)
+canvas.pack()
+img = tk.PhotoImage(file = "./photo.jpg")
+canvas.create_image(20,20,anchor=tk.NW,image=img)
+window.tk.call('wm','iconphoto',window._w,tk.PhotoImage(file="./slice.png"))
+window.geometry("800x500")
 window.mainloop()
